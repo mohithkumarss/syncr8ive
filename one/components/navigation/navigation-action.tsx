12567c183e2c18c055f07d402e2430e53d2a2283
Mohith Kumar SS <109334910+mohithkumarss@users.noreply.github.com>
2024-05-11 23:05:04 +0530

    till now

--- one/components/navigation/navigation-action.tsx
+++ one/components/navigation/navigation-action.tsx
@@ -14,7 +14,7 @@ export const NavigationAction = () => {
           onClick={() => onOpen("createServer")}
           className="group flex items-center"
         >
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-[#444560] group-hover:bg-[#3B3C51]">
+          <div className="flex m-3 h-[48px] w-[48px] rounded-[8px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-[#444560] group-hover:bg-[#3B3C51]">
             <Plus
               className="group-hover:text-white trasition text-blue"
               size={25}
